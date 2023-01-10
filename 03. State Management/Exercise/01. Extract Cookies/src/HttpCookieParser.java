d3d1d65a94b6e2b73807bdbe474afd4e1dfc4520
Daniel Baykov <danibaikov@abv.bg>
2023-01-10 12:10:22 +0200

    adding psfs variables and enum HttpMethod

---
+++ 03. State Management/Exercise/01. Extract Cookies/src/HttpCookieParser.java
@@ -0,0 +1,52 @@
+import java.util.Set;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class HttpCookieParser {
+
+    private static final Logger LOGGER = Logger.getLogger(HttpCookieParser.class.getName());
+
+    private static final String HEADER_SEPARATOR = ": ";
+    private static final String REQUEST_METHOD = "method";
+    private static final String REQUEST_RESOURCE = "resource";
+    private static final String REQUEST_HTTP_VERSION = "version";
+    private static final String HEADER_DATE = "Date";
+    private static final String HEADER_HOST = "Host";
+    private static final String HEADER_CONTENT_TYPE = "Content-Type";
+    private static final String HEADER_AUTHORIZATION = "Authorization";
+    private static final String HEADER_COOKIE = "Cookie";
+    private static final String HTTP_1_1 = "HTTP/1.1";
+    private static final String KEY = "key";
+    private static final String VALUE = "value";
+
+    private static final Pattern REQUEST_LINE_PATTERN = Pattern.compile(String.format(
+            "^(?<%s>[A-Z]{3,7}) (?<%s>/[a-zA-Z0-9/]+) (?<%s>HTTP/[0-9.]+)$",
+            REQUEST_METHOD, REQUEST_RESOURCE, REQUEST_HTTP_VERSION));
+
+    private static final Pattern BODY_PARAMS_PATTERN = Pattern.compile(String.format(
+            "&?(?<%s>[A-Za-z0-9]+)=(?<%s>[A-Za-z0-9]+)",
+            KEY, VALUE));
+
+    private static final Pattern HEADER_PATTERN = Pattern.compile(String.format(
+            "^(?<%s>[^ :]+)%s(?<%s>.+)$",
+            KEY, HEADER_SEPARATOR, VALUE));
+
+    private static final Pattern COOKIES_PATTERN = Pattern.compile("; ");
+
+    private static final Pattern COOKIES_PAIR_PATTERN = Pattern.compile("=");
+
+    private static final Set<String> VALID_HEADERS = Set.of(
+            HEADER_DATE, HEADER_HOST, HEADER_CONTENT_TYPE, HEADER_AUTHORIZATION, HEADER_COOKIE);
+
+    private static final Set<String> HTTP_VERSIONS = Set.of(HTTP_1_1);
+
+    private static final Set<String> HTTP_METHODS = Stream.of(HttpMethod.values())
+            .map(Enum::name)
+            .collect(Collectors.toUnmodifiableSet());
+
+
+
+    private enum HttpMethod {GET, POST}
+}
